0b4f1f5d6756c8c0a1ce8588a4912569c56bf738
muratilgun <muratilgun34@gmail.com>
2021-10-14 23:08:52 +0300

    Added columns to Identity Table

---
+++ Taste.Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+
+namespace Taste.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Display(Name = "Full Name")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [NotMapped]
+        public string FullName { get { return FirstName + " " + LastName; } }
+    }
+}
