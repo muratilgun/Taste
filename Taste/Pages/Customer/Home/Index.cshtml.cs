109a8f55752f7c95ed74a75289757d5e30c9a2e9
muratilgun <muratilgun34@gmail.com>
2021-10-20 01:11:37 +0300

    Update Index.cshtml.cs

--- Taste/Pages/Customer/Home/Index.cshtml.cs
+++ Taste/Pages/Customer/Home/Index.cshtml.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Taste.DataAccess.Data.Repository.IRepository;
 using Taste.Models;
+using Taste.Utility;
 
 namespace Taste.Pages.Customer.Home
 {
@@ -22,6 +25,15 @@ namespace Taste.Pages.Customer.Home
         public IEnumerable<Category> CategoryList { get; set; }
         public void OnGet()
         {
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (claim != null )
+            {
+                int shoppingCartCount = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).ToList()
+                    .Count();
+                HttpContext.Session.SetInt32(SD.ShoppingCart, shoppingCartCount);
+            }
             MenuItemList = _unitOfWork.MenuItem.GetAll(null,null,"Category,FoodType");
             CategoryList = _unitOfWork.Category.GetAll(null, q => q.OrderBy(c => c.DisplayOrder), null);
         }
