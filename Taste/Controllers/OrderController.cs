56458e86127c86576d9fe0becbbf127ebdef2c43
muratilgun <muratilgun34@gmail.com>
2021-10-24 23:11:36 +0300

    added order history for customers

--- Taste/Controllers/OrderController.cs
+++ Taste/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -24,6 +25,7 @@ namespace Taste.Controllers
         }
 
         [HttpGet]
+        [Authorize]
         public IActionResult Get(string status = null)
         {
             List<OrderDetailsViewModel> orderListVM = new List<OrderDetailsViewModel>();
